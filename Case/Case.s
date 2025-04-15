e59185fb53eb5561e823ae7642949730597f2f73
João Fidellis <jrfidellis@gmail.com>
2025-04-15 19:56:37 -0300

    updaet

--- Case/Case.s
+++ Case/Case.s
@@ -1,79 +1,69 @@
 //
 // Create assembly code to emulate a switch/case statement
-
+//
+// x: 64bits register
+// w: 32bits register
+//
 // REGISTERS USED IN CODE
 // w11 	- 	holds switch variable (1 thru 3 for this case statement)
 // w12 	-	holds the exit value that can be queried at the OS level with: echo $?
 //			NOTE:  w12 is transferred to w0 just before program exit so the
 //				   user can query the value with $?
 // w13 	-	work register used during calculation of mesg length
-
-// X0 - X2  hold parameters for Darwin/kernel system call
+//
 // X0	-	holds FD (file device) for output (stdout in this case)
 // X1	-	holds address of mesg
 // X2 	-	holds length of mesg 
-
+//
 // X16	-	used to hold Darwin/Kernel system call ID
 // X9	-	holds calculated length of mesg
 
 .global _start // Provide program starting address
 .align 4
 
-_start:		// this is the switch portion of the case statement
-			// branching to select1, select2, select3 or default
-			// labels based on value in w11
-			
-			// set "select" value (1 thru 3) in w11
-				
-			mov w12, 0xff	// Prepare for error case 
-			cmp x0, #2	// Make sure we have precisely two arguments
-			bne endit	// If it is not: exit
-			ldr x11, [x1, #8]	// Get the pointer at x1 + 8
-			ldrb w11, [x11]	// Load the Byte pointed to by that pointer into w11
-			sub w11, w11, #'0' // Subtract the ascii value for '0'
+.text
+_start:
+			mov w12, #255 // Prepare for error case 
+			cmp x0, #2	  // x0: quantity of arguments passed to program, cmp: compare
+			bne endit	  // Neander: jnz (jump not zero)
 
-			cmp w11, #1			// will set Z flag to 1 if w11 - 1 == 0
-			b.eq select1		// Z Flag == 1 ?
-			cmp w11, #2			// will set Z flag to 1 if w11 - 2 == 0
-			b.eq select2		// Z Flag == 1 ?
-			cmp w11, #3			// will set Z flag to 1 if w11 - 3 == 0
-			b.eq select3		// Z Flag == 1 ?
-			
-			// if w11 contains anything other than 1 thru 3 the program
-			// will fall thru to the default label here
-			
-			// each label is a switch/case target based on the above select code
-			
-default:	mov w12, #99					// Use 99 return code for os query ($?)
-			b break							// same as switch/case break statement
+			ldr x11, [x1, #8]	// Get a pointer to the second argument passed to the program. First 8 bytes are the program name, second 8 bytes are the first argument
+			ldrb w11, [x11]	    // Load the Byte pointed to by that pointer into w11
+			sub w11, w11, #'0'  // Subtract the ascii value for '0'
 
-select1:	mov w12, #1						// Use 1 return code for os query ($?)
-			b break							// same as switch/case break statement
+			cmp w11, #1         // Z = 1 if w11 == 1
+			b.eq select1        // jz
+			cmp w11, #2         
+			b.eq select2
+			cmp w11, #3         
+			b.eq select3
 			
-select2:	mov w12, #2						// Use 2 return code for os query ($?)
-			b break							// same as switch/case break statement
+			mov w12, #99
+			b break							
 
-select3:	mov w12, #3						// Use 3 return code for os query ($?)
-											// b break not necessary here as it will
-											// fall thru when done executing the
-											// select3: case
+select1:	mov w12, #1
+			b break							
+			
+select2:	mov w12, #4
+			b break							
+
+select3:	mov w12, #9
+			b break
 		
-break:		nop								// nop here just to prove we actually 
-											// wind up here from each case statement
-											// when debugging with lldb
-											
-											// code after the select/case would go 
-											// here
-											
-			ADRP	X1, mesg@PAGE 			// start of message to display at OS level
-			ADD	X1, X1, mesg@PAGEOFF
-			
-			// calculate length of mesg (store it in x9)
+break:
+			// Code for microcrontroller:
+			// LDR X2, =mesg_code        // Load the address of 'mesg_code' from the data section
+			// LDR X1, [X2]              // Load the actual value from the data section into register X1
 
-			mov x9, #0						// zero out x9 before starting
+			// Move the message into data memory area before manipulating
+			ADRP	X1, mesg_code@PAGE
+			ADD	X1, X1, mesg_code@PAGEOFF
+			
+			// Calculate length of mesg (store it in x9)
+			mov x9, #0						// x9 = 0
 cloop:
-			ldrb w13, [x1,x9]				// get the next digit in mesg
-			cmp  w13, #255					// is it equal to 255 (0xFF)
+			ldrb w13, [x1,x9]				// get the next byte in mesg
+			cmp  w13, #255					// is it equal to 255 (0xFF)?
 			b.eq  cend						// yes - jump to cend
 			add x9, x9, #1					// no  - increase x9 count by 1
 			b cloop							// do it again
@@ -81,26 +71,21 @@ cend:
 											// Setup the parameters to print string
 											// and then call Darwin/kernel to do it.
 			MOV	X0, #1	    				// 1 = StdOut
-
 			MOV	X2, X9	    				// length of our string
 			MOV	X16, #4	    				// Darwin write system call
 			SVC	#0x80 	    				// Call Darwin/kernel to output the string
 
-
-// Setup the parameters to exit the program
-// and then call the kernel to do it.
 endit:
 			mov		W0, W12					// move return code into W0 so it can be
 											// queried at OS level
     		MOV     X16, #1     			// System call number 1 terminates this program
     		SVC     #0x80           		// Call Darwin/kernel to terminate the program
-		
-			// return 0
 
 .data
-mesg:	.byte 0x1B												// clear screen 
-		.byte 'c'												// and start msg at
-		.byte 0													// top of screen
+mesg_code:	// Message in data memory section
+		.byte 0x1B
+		.byte 'c'
+		.byte 0
 		.asciz	"Emulate switch/case in assembly code\n\n"
 		.asciz	"Use: echo $? to see result of program\n"
-		.byte	255	
+		.byte	255
