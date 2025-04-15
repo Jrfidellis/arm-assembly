bd1fef2e79cac13ddde6a3c6696daed5060d91f1
João Fidellis <jrfidellis@gmail.com>
2025-04-15 20:32:34 -0300

    less comments

--- Case/Case.s
+++ Case/Case.s
@@ -27,7 +27,7 @@ _start:
 			cmp x0, #2	  // x0: quantity of arguments passed to program, cmp: compare
 			bne endit	  // Neander: jnz (jump not zero)
 
-			ldr x11, [x1, #8]	// Get a pointer to the second argument passed to the program. First 8 bytes are the program name, second 8 bytes are the first argument
+			ldr x11, [x1, #8]	// Gets the second argument passed to the program. First 8 bytes are the program name, second 8 bytes are the first argument
 			ldrb w11, [x11]	    // Load the Byte pointed to by that pointer into w11
 			sub w11, w11, #'0'  // Subtract the ascii value for '0'
 
@@ -39,47 +39,39 @@ _start:
 			b.eq select3
 			
 			mov w12, #99
-			b break							
+			b break
 
 select1:	mov w12, #1
-			b break							
+			b break			
 			
 select2:	mov w12, #4
-			b break							
+			b break
 
 select3:	mov w12, #9
 			b break
-		
-break:
-			// Code for microcrontroller:
-			// LDR X2, =mesg_code        // Load the address of 'mesg_code' from the data section
-			// LDR X1, [X2]              // Load the actual value from the data section into register X1
 
-			// Move the message into data memory area before manipulating
-			ADRP	X1, mesg_code@PAGE
-			ADD	X1, X1, mesg_code@PAGEOFF
+break:
+			adrp	X1, mesg_code@PAGE
+			add	X1, X1, mesg_code@PAGEOFF
 			
 			// Calculate length of mesg (store it in x9)
-			mov x9, #0						// x9 = 0
+			mov x9, #0
 cloop:
 			ldrb w13, [x1,x9]				// get the next byte in mesg
-			cmp  w13, #255					// is it equal to 255 (0xFF)?
-			b.eq  cend						// yes - jump to cend
-			add x9, x9, #1					// no  - increase x9 count by 1
-			b cloop							// do it again
-cend:	
-											// Setup the parameters to print string
-											// and then call Darwin/kernel to do it.
-			MOV	X0, #1	    				// 1 = StdOut
-			MOV	X2, X9	    				// length of our string
-			MOV	X16, #4	    				// Darwin write system call
-			SVC	#0x80 	    				// Call Darwin/kernel to output the string
+			cmp  w13, #255					// is it 255?
+			b.eq  cend						
+			add x9, x9, #1					// count++
+			b cloop							
+cend:
+			mov	X0, #1	    				// 1 = StdOut
+			mov	X2, X9	    				// length of our string
+			mov	X16, #4	    				// 4 = write syscall 
+			svc	#0x80 	    				// Call system call
 
 endit:
-			mov		W0, W12					// move return code into W0 so it can be
-											// queried at OS level
-    		MOV     X16, #1     			// System call number 1 terminates this program
-    		SVC     #0x80           		// Call Darwin/kernel to terminate the program
+			mov		W0, W12					// W0 stores exit code
+    		mov     X16, #1     			// 1 = exit syscall
+    		svc     #0x80           		// Call system call
 
 .data
 mesg_code:	// Message in data memory section
